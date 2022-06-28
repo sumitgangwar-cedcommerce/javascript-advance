765ab328628ccb11736d18b1496ea8fac2552e23
sumitgangwar-cedcommerce <104122327+sumitgangwar-cedcommerce@users.noreply.github.com>
2022-06-28 10:00:42 +0530

    Task 3

--- script.js
+++ script.js
@@ -1,51 +1,30 @@
-var arr = [];
-var Arr;
-class Employee {
-   constructor(ID, Name, Sal) {
-      this.ID = ID;
-      this.Name = Name;
-      this.Sal = Sal;
-   }
-
-}
-var temp=1;
-
-function check() {
-   
-    var id = null;
-    id = document.getElementById("ID").value;
-    var name = document.getElementById("Name").value;
-    var sal = null;
-    sal = document.getElementById("Sal").value;
-    var temp=1;
-    
-    try{
-      if(id=="" || name=="" || sal=="")  throw "All fields are compulsary";
-      if(!isNaN(name))  throw "Name should not be a number";
-    } 
-    catch (e) {
-      temp=0;
-      document.getElementById("arrow").innerHTML = e;
+var  arr =[];
+var  Arr;
+class Employee{
+    constructor(ID,Name,Sal){
+        this.ID=ID;
+        this.Name=Name;
+        this.Sal=Sal;
     }
-   if(temp){
-    document.getElementById("arrow").innerHTML = "";
-    let E1 = new Employee(id, name, sal)
-    Arr = JSON.stringify(E1);
+}
+function check(){
+    var id=document.getElementById("ID").value;
+    var name=document.getElementById("Name").value;
+    var sal=document.getElementById("Sal").value;
+    let E1= new Employee(id,name,sal);
+    Arr=JSON.stringify(E1);
     arr.push(Arr);
-    show();
-   }
+    Show();    
 }
 
-function show() {
-  
-   let text = "<table><th>Id </th><th>Name</th><th>Basic Salary</th><th>Action<th>";
-   for (let j = 0; j < arr.length; j++) {
-      let t = JSON.parse(arr[j]);
-
-      text += "<tr><td>" + t.ID + "</td><td>" + t.Name + "</td><td>" + t.Sal + "</td><td><button>Delete</button></tr>"
-   }
-
-
-   text += "</table>";
-   document.getElementById("res").innerHTML = text;
+function Show(){
+    console.log(arr);
+    let text="<table><th>Id  </th><th>Name</th><th>Basic Salary</th><th>Action<th>";
+    for(j=0;j<arr.length;j++){
+        let temp = JSON.parse(arr[j])
+        text+= "<tr><td>"+temp.ID+"</td><td>"+temp.Name+"</td><td>"+temp.Sal+"</td><td><button>Delete</button></tr>"
+    }
+    text+= "</table>";
+    document.getElementById("res").innerHTML=text;
 }
+
